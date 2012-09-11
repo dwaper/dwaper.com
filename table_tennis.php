3f5b15d6675986d49f903efd8879723a6fa875ff
mirobeka <mirobeka@gmail.com>
2012-09-11 16:11:01 +0200

    new games

---
+++ table_tennis.php
@@ -0,0 +1,20 @@
+<?php
+  require_once('AppInfo.php')
+?>
+
+<html>
+  <head>
+    <meta prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# dwaper-php-dev: http://ogp.me/ns/fb/dwaper-php-dev#">
+    <meta property="fb:app_id" content="411022408959008" /> 
+    <meta property="og:type"   content="dwaper-php-dev:game" /> 
+    <meta property="og:url"    content="<?php echo AppInfo::getUrl(); ?>table_tennis.php" /> 
+    <meta property="og:title"  content="Table Tennis" /> 
+    <meta property="og:image"  content="<?php echo AppInfo::getUrl(); ?>images/icon_table_tennis.png" />  
+    <title>Frisbee</title>
+  </head>  
+
+  <body>
+    <p> Some information to display about the frisbee</p>
+  </body>
+</html>
+
